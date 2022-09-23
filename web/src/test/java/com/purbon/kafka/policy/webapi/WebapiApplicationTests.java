533d2c87fa904ae10c12ca27002f650e9f7d1bfd
Pere Urbon <pere@confluent.io>
2022-09-23 16:25:25 +0200

    add sckelethon for a web api

---
+++ web/src/test/java/com/purbon/kafka/policy/webapi/WebapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purbon.kafka.policy.webapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
